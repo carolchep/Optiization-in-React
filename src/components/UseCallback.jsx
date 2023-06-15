b70e494014e067dbeccac0c428c6d45b11124a0a
carolchep <carolchep@gmail.com>
2023-06-15 13:38:07 +0300

    bug0fix

--- src/components/UseCallback.jsx
+++ src/components/UseCallback.jsx
@@ -5,19 +5,19 @@ const Child = ({ onClick }) => {
   return <button onClick={onClick}>Click me</button>;
 };
 
-const Parent = () => {
-  const [count, setCount] = useState(0);
+// const Parent = () => {
+//   const [count, setCount] = useState(0);
 
-  const handleClick = useCallback(() => {
-    console.log('Button clicked');
-    setCount(count + 1);
-  }, [count]);
+//   const handleClick = useCallback(() => {
+//     console.log('Button clicked');
+//     setCount(count + 1);
+//   }, [count]);
 
-  return (
-    <div>
-      <Child onClick={handleClick} />
-      <p>{count}</p>
-    </div>
+//   return (
+//     <div>
+//       <Child onClick={handleClick} />
+//       <p>{count}</p>
+//     </div>
   );
 };
 //In this example, the handleClick function is only re-created when the count state changes.
